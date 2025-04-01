aae16abec2eaca762a1de38aeaaaedf1a560c05d
ErickS3 <esalva03@calpoly.edu>
2025-04-01 10:29:26 -0700

    this is a commit message

--- Lab00.java
+++ Lab00.java
@@ -2,7 +2,12 @@ public class Lab00
 {
    public static void main(String[] args)
    {
+      int x = 5;
+      String y = "hello";
+      float z = 9.8F;
 
-   
+      System.out.println("x:"+ x + "y:" + y + "z:" + z);
+
+
    }
 }
