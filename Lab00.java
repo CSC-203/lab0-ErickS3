04f6849f54bd943f2bf0e09de3daaa37f5d1ad10
ErickS3 <esalva03@calpoly.edu>
2025-04-02 12:18:22 -0700

    this is a commit message

--- Lab00.java
+++ Lab00.java
@@ -3,25 +3,45 @@ public class Lab00
 {
    public static void main(String[] args)
    {
+      //declaring and initializing variables
       int x = 5;
       String y = "hello";
       float z = 9.8F;
 
+
+      //printing the variables
       System.out.println("x: " + x + " y: " + y + " z: " + z);
 
-      int[] nums = {3, 6, -1, 2};
 
+      //a list of integers
+      int[] nums = {3, 6, -1, 2};
       for(int num : nums) {
          System.out.println(num);
       }
 
 
+      //calling a function
+      int numFound = char_count(y, "l");
+      System.out.println("Found:" +  numFound);
+
+
+      //a counting for loop
       for(int i = 1; i < 11; i++){
          System.out.print(i + " ");
       }
 
+   }
 
+   public static int char_count(String s, String c){
+      int count = 0;
+      for(String ch : s){
+         if(ch.equals(c)){
+            count = count + 1;
 
+         }
+         System.out.println(count);
+      }
 
    }
+
 }
